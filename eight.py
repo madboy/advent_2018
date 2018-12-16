da9ab083128a5cccd4d8f964aab7823919fef0ef
Kristofer Lindberg <k.lindberg@gmail.com>
2018-12-16 18:45:28 +0100

    the days are counting

--- eight.py
+++ eight.py
@@ -2,35 +2,40 @@
 
 import fileinput
 
-class Node(object):
-    def __init__(self, c, m):
-        self.c = int(c)
-        self.m = int(m)
-
-    def __str__(self):
-        return "({}, {})".format(self.c, self.m)
-
-    def __repr__(self):
-        return self.__str__()
-
+def dfs_visit_2(tree, u, metadata):
+    n, m = tree[u:u+2]
+    my_metadata = []
+    for _ in range(0, n):
+        u = dfs_visit_2(tree, u+2, my_metadata)
+    if n == 0:
+        metadata.append(sum(tree[u+2:u+2+m]))
+    else:
+        total = 0
+        meta = tree[u+2:u+2+m]
+        for e in meta:
+            try:
+                total += my_metadata[e-1]
+            except IndexError:
+                pass
+        metadata.append(total)
+    return u + m
 
 def dfs_visit(tree, u, metadata):
     n, m = tree[u:u+2]
-    n = int(n)
-    m = int(m)
     for _ in range(0, n):
         u = dfs_visit(tree, u+2, metadata)
-    metadata.append(tree[u+2:u+2+m])
+    metadata.append(sum(tree[u+2:u+2+m]))
     return u + m
 
 tree = fileinput.input().readline().strip().split(" ")
+for i, t in enumerate(tree):
+    tree[i] = int(t)
+
 metadata = []
 
 dfs_visit(tree, 0, metadata)
-total = 0
-for e in metadata:
-    for m in e:
-        total += int(m)
+print(sum(metadata))
 
-# print(total)
-print(total, metadata)
+metadata2 = []
+dfs_visit_2(tree, 0, metadata2)
+print(metadata2.pop())
