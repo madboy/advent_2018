28431d53aaf0053253a05ba45f560795cabd1a95
Kristofer Lindberg <k.lindberg@gmail.com>
2018-12-19 17:59:51 +0100

    not nice, but now automatic

--- six.py
+++ six.py
@@ -7,106 +7,61 @@ import string
 Coord = namedtuple('Coord', ['n', 'x', 'y'])
 coords = []
 bounded = []
-# alpha = list(string.ascii_lowercase)
 alpha = list(string.ascii_letters)
 
 max_value = 0
 for line in fileinput.input():
-    x, y = line.strip().split(",")
-    x = int(x)
-    y = int(y)
+    x, y = map(int, line.strip().split(","))
     if max(x, y) > max_value:
         max_value = max(x, y)
     coords.append(Coord(alpha.pop(0), x, y))
 
-# print(coords)
 max_value += 1
 
-# NB: current calculation isn't correct
-# you need to have intersections to be
-# able to have a closed area
-for c in coords:
-    left, right, up, down = False, False, False, False
-    intersect = []
-    for cc in coords:
-        if c == cc:
-            continue
-        if cc.x < c.x and cc.y < c.y: # lower right
-            left = True
-            intersect.append(cc)
-            # print("x intersection left for {} and {}".format(c.n, cc.n))
-        if cc.x < c.x and cc.y > c.y: # upper right
-            # print("x intersection right for {} and {}".format(c.n, cc.n))
-            right = True
-            intersect.append(cc)
-        if cc.x > c.x and cc.y < c.y: # lower left
-            # print("y intersection up for {} and {}".format(c.n, cc.n))
-            up = True
-            intersect.append(cc)
-        if cc.x > c.x and cc.y > c.y: # upper left
-            # print("x intersection down for {} and {}".format(c.n, cc.n))
-            down = True
-            intersect.append(cc)
-    if left and right and up and down:
-        bounded.append(c.n)
-        # print("{} is bounded by {}".format(c, intersect))
+def closest_char(x, y, coords, max_value):
+    mind = max_value*max_value
+    char = "."
+    for coord in coords:
+        d = abs(coord.x - x) + abs(coord.y - y)
+        if d == mind:
+            char = "."
+        elif d < mind:
+            char = coord.n
+            mind = d
+    return char
 
-# line = ""
-# counts = defaultdict(int)
-# for y in range(0, max_value):
-#     for x in range(0, max_value):
-#         mind = max_value*max_value
-#         char = "."
-#         for coord in coords:
-#             d = abs(coord.x - x) + abs(coord.y - y)
-#             if d == mind:
-#                 char = "."
-#                 # mind = d
-#             elif d < mind:
-#                 char = coord.n
-#                 mind = d
-#         counts[char] += 1
-        # line = line + char
-    # line = line + "\n"
-# print(line)
+counts = defaultdict(int)
+for y in range(0, max_value):
+    for x in range(0, max_value):
+        char = closest_char(x, y, coords, max_value)
+        counts[char] += 1
 
-# 5333, b
-# print(counts)
-# print(max(counts.values()))
+# check if area expands if we go outside of box
+infinite = set()
+for y in [-2, -1, max_value+1, max_value+2]:
+    for x in range(0, max_value):    
+        char = closest_char(x, y, coords, max_value)
+        infinite.add(char)
 
-# max_area = 0
-# choice = -1
-# for b in bounded:
-#     area = counts[b]
-#     if area > max_area:
-#         max_area = area
-#         choice = b
+for y in range(0, max_value):
+    for x in [-2, -1, max_value+1, max_value+2]: 
+        char = closest_char(x, y, coords, max_value)
+        infinite.add(char)
 
-# for b in coords:
-#     area = line.count(b.n)
-#     if area > max_area:
-#         max_area = area
-#         choice = b.n
+largest_area = 0
+for k, v in counts.items():
+    if k not in infinite and v > largest_area:
+        largest_area = v
 
-# print(max_value, choice, max_area)
-
+print(largest_area)
 
 area = 0
-# counts = defaultdict(int)
 for y in range(-10, max_value+10):
     for x in range(-10, max_value+10):
-        # mind = max_value*max_value
-        # char = "."
         sumd = 0
         for coord in coords:
             sumd += abs(coord.x - x) + abs(coord.y - y)
-            # if d == mind:
-                # char = "."
-                # mind = d
-            # elif d < mind:
-                # char = coord.n
-                # mind = d
         if sumd < 10000:
             area += 1
 
-print(area) # 35334
+print(area)
