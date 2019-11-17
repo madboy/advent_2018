a174492635a5b2c7483d48db87083414fb7ca970
Kristofer Lindberg <k.lindberg@gmail.com>
2019-11-17 14:31:55 +0100

    working if slow version of part 2 day 11

--- src/eleven.py
+++ src/eleven.py
@@ -1,6 +1,8 @@
 """
-Given a 300x300 grid of fuel cells select the 3x3 cell square that has the largest total power
+Given a 300x300 grid of fuel cells select the 3x3 (1) or variable size (2) cell 
+square that has the largest total power
 """
+import math
 from src.tools import process
 
 
@@ -9,19 +11,21 @@ def power_level(x, y, grid_serial_number):
     power_lvl = rack_id * y
     power_lvl += grid_serial_number
     power_lvl *= rack_id
-    # keep only the hundreds digit, if none 0
-    try:
-        power_lvl = int(str(power_lvl)[-3])
-    except IndexError:
-        power_lvl = 0
+    # keep only the hundreds digit, if below 100 -> 0
+    power_lvl = int(power_lvl / 100) % 10
     return power_lvl - 5
 
 
-def compute_square_power_level(x, y, grid, grid_size):
-    total_power_level = 0
+def compute_square_power_level(x, y, grid, grid_size, computed_grid):
+    # use a successively computed grid and only look at new column and row
+    total_power_level = computed_grid.get((x, y), 0)
     for i in range(x, x + grid_size):
-        for ii in range(y, y + grid_size):
-            total_power_level += grid[(i, ii)]
+        total_power_level += grid[(i, y + grid_size - 1)]
+
+    # avoid duplicate of coord that has already been covered above by not going to last y
+    for i in range(y, y + grid_size - 1):
+        total_power_level += grid[(x + grid_size - 1, i)]
+    computed_grid[(x, y)] = total_power_level
     return total_power_level
 
 
@@ -37,12 +41,14 @@ def find_largest_power_level(grid, smallest_grid_size, largest_grid_size):
     top_left = (1, 1)
     current_spl = -1000
     best_gs = 1
-    for x in range(1, 298):
-        for y in range(1, 298):
+    size = int(math.sqrt(len(grid.keys())))
+    computed_grid = {}
+    for x in range(1, size + 1):
+        for y in range(1, size + 1):
             for gs in range(smallest_grid_size, largest_grid_size):
-                if x + gs > 300 or y + gs > 300:
+                if (x + gs - 1) > size or (y + gs - 1) > size:
                     continue
-                spl = compute_square_power_level(x, y, grid, gs)
+                spl = compute_square_power_level(x, y, grid, gs, computed_grid)
                 if spl > current_spl:
                     top_left = (x, y)
                     current_spl = spl
--- test/eleven_test.py
+++ test/eleven_test.py
@@ -1,4 +1,9 @@
-from src.eleven import power_level, find_largest_power_level, populate_grid
+from src.eleven import (
+    power_level,
+    find_largest_power_level,
+    populate_grid,
+    compute_square_power_level,
+)
 import unittest
 
 
@@ -9,30 +14,84 @@ class DayElevenTests(unittest.TestCase):
         self.assertEqual(0, power_level(217, 196, 39))
         self.assertEqual(4, power_level(101, 153, 71))
 
+    def test_compute_square_power_level(self):
+        grid = {
+            (1, 1): 1,
+            (1, 2): 1,
+            (2, 1): 1,
+            (2, 2): 1,
+        }
+        computed_grid = {
+            (1, 1): 1,
+            (1, 2): 1,
+            (2, 1): 1,
+            (2, 2): 1,
+        }
+
+        r = compute_square_power_level(1, 1, grid, 2, computed_grid)
+        self.assertEqual(4, r)
+
+    def test_compute_square_power_level_successive(self):
+        grid = {
+            (1, 1): 1,
+            (1, 2): 1,
+            (1, 3): 1,
+            (2, 1): 1,
+            (2, 2): 1,
+            (2, 3): 1,
+            (3, 1): 1,
+            (3, 2): 1,
+            (3, 3): 1,
+        }
+        computed_grid = {}
+
+        r = compute_square_power_level(1, 1, grid, 1, computed_grid)
+        self.assertEqual(1, r)
+
+        r = compute_square_power_level(1, 1, grid, 2, computed_grid)
+        self.assertEqual(4, r)
+
+        r = compute_square_power_level(1, 1, grid, 3, computed_grid)
+        self.assertEqual(9, r)
+
     def test_square_power_level_18(self):
         grid = populate_grid(18)
-        top_left, pl, gs = find_largest_power_level(grid, 3, 4)
+        top_left, pl, gs = find_largest_power_level(grid, 1, 4)
         self.assertEqual((33, 45), top_left)
         self.assertEqual(29, pl)
         self.assertEqual(3, gs)
 
     def test_square_power_level_42(self):
         grid = populate_grid(42)
-        top_left, pl, gs = find_largest_power_level(grid, 3, 4)
+        top_left, pl, gs = find_largest_power_level(grid, 1, 4)
         self.assertEqual((21, 61), top_left)
         self.assertEqual(30, pl)
         self.assertEqual(3, gs)
 
-    # def test_square_power_level_18_variable_size(self):
-    #     grid = populate_grid(18)
-    #     top_left, pl, gs = find_largest_power_level(grid, 1, 300)
-    #     self.assertEqual((90, 269), top_left)
-    #     self.assertEqual(113, pl)
-    #     self.assertEqual(16, gs)
+    def test_find_largest_power_level_ones(self):
+        grid = {
+            (1, 1): 1,
+            (1, 2): 1,
+            (2, 1): 1,
+            (2, 2): 1,
+        }
+        top_left, pl, gs = find_largest_power_level(grid, 1, 3)
+        self.assertEqual((1, 1), top_left)
+        self.assertEqual(2, gs)
+        self.assertEqual(4, pl)
 
-    # def test_square_power_level_42_variable_size(self):
-    #     grid = populate_grid(42)
-    #     top_left, pl, gs = find_largest_power_level(grid, 1, 300)
-    #     self.assertEqual((232, 251), top_left)
-    #     self.assertEqual(119, pl)
-    #     self.assertEqual(12, gs)
+    @unittest.skip
+    def test_square_power_level_18_variable_size(self):
+        grid = populate_grid(18)
+        top_left, pl, gs = find_largest_power_level(grid, 1, 300)
+        self.assertEqual((90, 269), top_left)
+        self.assertEqual(113, pl)
+        self.assertEqual(16, gs)
+
+    @unittest.skip
+    def test_square_power_level_42_variable_size(self):
+        grid = populate_grid(42)
+        top_left, pl, gs = find_largest_power_level(grid, 1, 300)
+        self.assertEqual((232, 251), top_left)
+        self.assertEqual(119, pl)
+        self.assertEqual(12, gs)
