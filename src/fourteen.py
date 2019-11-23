0970edc4125c63047fac6e18390a2e3a4cf5fbf1
Kristofer Lindberg <k.lindberg@gmail.com>
2019-11-23 21:22:18 +0100

    minor improvement

--- src/fourteen.py
+++ src/fourteen.py
@@ -4,6 +4,7 @@ from src.tools import process
 def run(input_file):
     for number in process(input_file):
         print(number)
+        number_len = len(number)
 
         e1 = 0
         e2 = 1
@@ -17,19 +18,19 @@ def run(input_file):
             result = str(recipe1 + recipe2)
             current += result
 
-            new = [int(c) for c in str(recipe1 + recipe2)]
+            new = [int(c) for c in result]
             results.extend(new)
 
             if number in current:
                 break
             else:
-                current = current[-len(number) :]
+                current = current[-number_len:]
 
             e1 = (recipe1 + e1 + 1) % len(results)
             e2 = (recipe2 + e2 + 1) % len(results)
 
         # if our number isn't the last part of the current result we need
         # to subtract one extra from how many recipes are before our number
-        adjustment = 0 if current[-len(number) :] == number else 1
-        print(len(results) - len(number) - adjustment)
+        adjustment = 0 if current[-number_len:] == number else 1
+        print(len(results) - number_len - adjustment)
 
