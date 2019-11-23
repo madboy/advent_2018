6bb50fd6143a766d79298f40ca66d3d950b6185d
Kristofer Lindberg <k.lindberg@gmail.com>
2019-11-23 11:47:04 +0100

    day 14 part 2

--- src/fourteen.py
+++ src/fourteen.py
@@ -3,21 +3,30 @@ from src.tools import process
 
 def run(input_file):
     for number in process(input_file):
-        number = int(number)
         print(number)
 
         e1 = 0
         e2 = 1
         results = [3, 7]
+        current = "37"
 
-        while len(results) < number + 10:
+        while True:
             recipe1 = results[e1]
             recipe2 = results[e2]
+
+            result = str(recipe1 + recipe2)
+            current += result
 
             new = [int(c) for c in str(recipe1 + recipe2)]
             results.extend(new)
 
-            e1 = (recipe1 + e1 + 1) % len(results)
-            e2 = (recipe2 + e2 + 1) % len(results)
+            if number in current:
+                break
+            else:
+                current = current[-len(number) :]
 
-        print("".join([str(n) for n in results[number : number + 10]]))
+            e1 = (recipe1 + e1 + 1) % len(results)
+
+        adjustment = 0 if current[-len(number) :] == number else 1
+        print(len(results) - len(number) - adjustment)
+
