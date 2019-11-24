f351d92b736d6c2a5ec0fa9f24f643a137ca640a
Kristofer Lindberg <k.lindberg@gmail.com>
2019-11-24 15:59:21 +0100

    Error handling on advent runner

--- advent.py
+++ advent.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from dataclasses import dataclass
 from src import (
     one,
     two,
@@ -16,29 +17,46 @@ from src import (
     fourteen,
 )
 import sys
+from typing import Callable
+
+@dataclass
+class Day:
+    program: Callable
+    input_file: str
 
 days = [
     (),
-    (one.run, "input/1"),
-    (two.run, "input/2"),
-    (three.run, "input/3"),
-    (four.run, "input/4"),
-    (five.run, "input/5"),
-    (six.run, "input/6"),
-    (seven.run, "input/7"),
-    (eight.run, "input/8"),
-    (nine.run, "input/9"),
-    (ten.run, "input/10"),
-    (eleven.run, "input/11"),
-    (twelve.run, "input/12"),
-    (thirteen.run, "input/13"),
-    (fourteen.run, "input/14"),
+    Day(one.run, "input/1"),
+    Day(two.run, "input/2"),
+    Day(three.run, "input/3"),
+    Day(four.run, "input/4"),
+    Day(five.run, "input/5"),
+    Day(six.run, "input/6"),
+    Day(seven.run, "input/7"),
+    Day(eight.run, "input/8"),
+    Day(nine.run, "input/9"),
+    Day(ten.run, "input/10"),
+    Day(eleven.run, "input/11"),
+    Day(twelve.run, "input/12"),
+    Day(thirteen.run, "input/13"),
+    Day(fourteen.run, "input/14"),
 ]
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: ./advent.py <day_nbr>")
+        sys.exit(1)
+
     try:
         day = int(sys.argv[1])
-        days[day][0](days[day][1])
-    except (IndexError, ValueError):
-        print("usage: ./advent.py <day_nbr>")
+    except ValueError:
+        print("Try using a number.")
+        sys.exit(2)
+
+    if day >= len(days) or day <= 0:
+        print(f"I don't know that day yet. Try one between 1-{len(days)-1}.")
+        sys.exit(3)
+
+    d = days[day]
+    d.program(d.input_file)
